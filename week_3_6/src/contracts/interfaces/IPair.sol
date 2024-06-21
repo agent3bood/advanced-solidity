aa086d49e5caf94358237188d7c014c629c8e3a9
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2024-06-21 12:31:20 +0300

    code review cleanup

--- week_3_6/src/contracts/interfaces/IPair.sol
+++ week_3_6/src/contracts/interfaces/IPair.sol
@@ -16,15 +16,15 @@ import "solady/tokens/ERC20.sol";
     function swap(uint amountAOut, uint amountBOut, address to) external;
 }*/
 
+pragma solidity ^0.8.0;
 
 interface IPair {
     /*********
     * Events *
     **********/
 
-    event Swap(address indexed sender, uint amountAIn, uint amountBIn, uint amountAOut, uint amountBOut, addressed indexed to);
+    event Swap(address indexed sender, uint amountAIn, uint amountBIn, uint amountAOut, uint amountBOut, address indexed to);
     event Sync(uint reserveA, uint reserveB);
-
 
     /***************************
     * State changing functions *
@@ -32,13 +32,15 @@ interface IPair {
 
     /// @notice Add liquidity by approving amountA and amountB
     /// @dev Mint liquidity by approving amountA and amountB
-    /// @param liquidity The minimum amount of liquidity to be minted
+    /// @param minLiquidity The minimum amount of liquidity to be minted
     /// @param amountA The approved amount for tokenA
     /// @param amountB The approved amount for tokenB
+    /// @param to The address to receive liquidity tokens
     /// @param deadline Transaction deadline
-    /// @param liquidity The minted liquidity amount
+    /// @return liquidity The minted liquidity amount
     function addLiquidity(
-        uint minLiquidity, uint amountA,
+        uint minLiquidity,
+        uint amountA,
         uint amountB,
         address to,
         uint deadline
@@ -49,25 +51,27 @@ interface IPair {
     /// @param liquidity The amount of liquidity to be burnt
     /// @param amountAMin The minimum desired amount of tokenA
     /// @param amountBMin The minimum desired amount of tokenB
+    /// @param to The address to receive the tokens
     /// @param deadline Transaction deadline
-    /// @param amountA The returned amount of tokenA
-    /// @param amountB The returned amount of tokenB
+    /// @return amountA The returned amount of tokenA
+    /// @return amountB The returned amount of tokenB
     function removeLiquidity(
         uint liquidity,
         uint amountAMin,
-        uint amountBMint,
+        uint amountBMin,
+        address to,
         uint deadline
     ) external returns(uint amountA, uint amountB);
 
     /// @notice Low level call to mint liquidity
     /// @param to The liquidity receiving address
-    /// @param liquidity The amount of liquidity minted
+    /// @return liquidity The amount of liquidity minted
     function mint(address to) external returns (uint liquidity);
 
     /// @notice Low level call to burn liquidity
     /// @param to The receiving address of tokens
-    /// @param amountA The amount received for tokenA
-    /// @param amountB The amount received for tokenB
+    /// @return amountA The amount received for tokenA
+    /// @return amountB The amount received for tokenB
     function burn(address to) external returns (uint amountA, uint amountB);
 
     /// @notice Swap exact amount of input token for not less than output amount of tokens
@@ -76,7 +80,7 @@ interface IPair {
     /// @param path The exchange path
     /// @param to The receiving address
     /// @param deadline Transaction deadline
-    /// @param amounts The exchanged amounts
+    /// @return amounts The exchanged amounts
     function swapExactTokensForTokens(
         uint amountIn,
         uint amountOutMin,
@@ -85,22 +89,24 @@ interface IPair {
         uint deadline
     ) external returns (uint[] memory amounts);
 
-    /// @notice Swap not less than amount of input token for exact output amount of tokens
+    /// @notice Swap tokens for an exact amount of output tokens
     /// @param amountOut The desired amount of output token
-    /// @param amountOutMin The minimum desired amount of output token
+    /// @param amountInMax The maximum amount of input token
     /// @param path The exchange path
     /// @param to The receiving address
-    /// @param amounts The exchanged amounts
+    /// @param deadline Transaction deadline
+    /// @return amounts The exchanged amounts
     function swapTokensForExactTokens(
         uint amountOut,
         uint amountInMax,
         address[] calldata path,
-        address to
+        address to,
+        uint deadline
     ) external returns(uint[] memory amounts);
 
     /// @notice Low level call to swap tokens
-    /// @param amountAOut the desired amount of tokenA
-    /// @param amountBOut the desired amount of tokenB
+    /// @param amountAOut The desired amount of tokenA
+    /// @param amountBOut The desired amount of tokenB
     /// @param to The receiving address of tokens
     function swap(uint amountAOut, uint amountBOut, address to) external;
 
@@ -109,20 +115,20 @@ interface IPair {
     *****************/
 
     /// @notice The first token in the pair
-    function tokenA() external view returns (ERC20);
+    function tokenA() external view returns (address);
 
-    /// @notice The secont token in the pair
-    function tokenB() external view returns (ERC20);
+    /// @notice The second token in the pair
+    function tokenB() external view returns (address);
 
     /// @notice The pair reserves
-    /// @param reserveA The reserve of tokenA
-    /// @param reserveB The reserve of tokenB
+    /// @return reserveA The reserve of tokenA
+    /// @return reserveB The reserve of tokenB
     function getReserves() external view returns(uint reserveA, uint reserveB);
 
-    /// @notice Given the input amount get the equivalent amount of the other token
+    /// @notice Given the input amount, get the equivalent amount of the other token
     /// @param amountA The amount of asset A
     /// @param reserveA The reserve amount of asset A
     /// @param reserveB The reserve amount of asset B
-    /// @param amountB The equivalent amount of asset B
+    /// @return amountB The equivalent amount of asset B
     function quote(uint amountA, uint reserveA, uint reserveB) external pure returns(uint amountB);
 }
